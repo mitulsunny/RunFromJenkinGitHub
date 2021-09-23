ee70994d3b87069f311529ada36a615c236d87e3
Md Obaidulla <mitulsunny8@gmail.com>
2021-09-23 17:05:35 -0400

    added third line

--- src/com/osa/test/MyTest.java
+++ src/com/osa/test/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
 public static void main(String[] args) {
 	System.out.println("hello world!!!");
 	System.out.println("Running from githut!!");
+	System.out.println("I am adding this third line to check the SCM poll is getting from Github repo!!");
 }
 }
